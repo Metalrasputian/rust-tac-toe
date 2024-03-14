5edd844d2800713d54cd3073e41df2b99f723f6b
Metalrasputian <64284928+Metalrasputian@users.noreply.github.com>
2024-03-14 06:00:17 +0000

    Mostly complete. Added random bot logic and game state flow.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
 use std::io;
 use std::convert::TryFrom;
+use rand::prelude::*;
 
 fn main () {
     let mut playing = true;
 
     while playing {
-
         let mut round_won = false;
 
-        //Uncomment once you make the starting board blank
-        //let mut board = [" "; 9];
+        let mut board = [" "; 9];
 
-        let mut board = [" ", "X", "O", "O", "X", " ", "X", "O", " "];
+        //Set up logic to change this to a coin flip for the user to be X or O
+        let player_token = "X";
+        let bot_token = "O";
 
-        while !round_won {            
-
+        while !round_won {
             println!("{}", render_screen(board));
 
             //Fix function to return Ok() and Err() instead of an int or 10 as an error
-            let move_index = get_player_move(board);
+            let move_index = get_player_move();
 
             if move_index < 10 {
-
                 //Better error catching here
                 let board_index = usize::try_from(move_index - 1).unwrap();
 
@@ -31,14 +30,29 @@ fn main () {
                     println!("{} is not a valid square. ({} is there).", move_index, cell_value);
                 }
                 else {
+                    board[board_index] = player_token;
+                }
 
-                    //Place symbol in array
+                if check_win(board, player_token){
+                    println!("Player wins!");
+                    round_won = true;
+                }
+                else {
+                    //Bot move currently ignores board state. Likely a scope/mutability issue.
+                    let bot_move = get_bot_move(board);
+
+                    board[bot_move] = bot_token;
+
+                    println!("Bot wins!");
+                    round_won = true;
                 }
             }
             else {
                 println!("Invalid input. Please enter a number from 1 to 9!");
             }
         }
+        //Print final board
+        println!("{}", render_screen(board));
 
         //Exit code
         let mut exit_answer = String::new();        
@@ -57,7 +71,6 @@ fn main () {
             }
         }
     }
-    
 }
 
 fn render_screen(board: [&str;9]) -> String {
@@ -73,13 +86,19 @@ fn render_screen(board: [&str;9]) -> String {
     screen
 }
 
-fn get_bot_move(board: [&str;9]) -> u32 {
-    let mut bot_move = 0;
+fn get_bot_move(board: [&str;9]) -> usize {
+    let mut valid_cells = Vec::<u32>::new();
 
-    bot_move
+    for i in 0..board.len() {
+        if board[usize::try_from(i).unwrap()] == " "{
+            valid_cells.push(u32::try_from(i).unwrap());
+        }
+    }
+
+    rand::thread_rng().gen_range(0..valid_cells.len())
 }
 
-fn get_player_move(board: [&str;9]) -> u32 {
+fn get_player_move() -> u32 {
     let mut player_move = String::new();
     println!("Enter your next square (1-9): ");
 
@@ -88,8 +107,32 @@ fn get_player_move(board: [&str;9]) -> u32 {
         .expect("No input detected!");
 
     //Returns 10 since that's out of range for our logic.
-    player_move.trim().parse::<u32>() {
+    match player_move.trim().parse::<u32>() {
         Ok(i) => i,
         Err(_) => 10,
     }
+}
+
+fn check_win(board: [&str;9], token: &str) -> bool {
+
+    //Check Rows
+    for y in 0..3 {
+        //Better parsing here
+        let check_index = usize::try_from(y).unwrap();
+
+        if board[check_index * 3] == token && board[(check_index * 3) + 1] == token && board[(check_index * 3) + 2] == token { return true; }
+    }
+
+    //Check Columns
+    for x in 0..3 {
+        //Better parsing here
+        let check_index = usize::try_from(x).unwrap();
+
+        if board[check_index] == token && board[check_index + 3] == token && board[check_index + 6] == token { return true; }
+    }
+
+    //Check Diagonals
+    if board[0] == token && board[4] == token && board[8] == token || board[2] == token && board[4] == token && board[6] == token { return true; }
+
+    false
 }
